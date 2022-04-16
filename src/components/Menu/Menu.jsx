08a6b03dceabfa0967dd28a582a1088ce5301daa
Daniel <danielatts97@hotmail.com>
2022-04-16 01:16:03 -0500

    create 404page

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import './Menu.styles.scss';
+import {
+  ROUTE_HOME, ROUTE_ABOUT,
+} from '../../routes/routes';
 
 function Menu() {
   return (
     <div className="menu">
-      <p>Hola, mundo</p>
+      <NavLink to={ROUTE_HOME} className="menu__navlink">HOME</NavLink>
+      <NavLink to={ROUTE_ABOUT} className="menu__navlink">ABOUT</NavLink>
     </div>
   );
 }
