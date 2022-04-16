277438e39192426e3d3302961d1599474c2d4fb2
Daniel <danielatts97@hotmail.com>
2022-04-16 12:32:53 -0500

    create product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,21 @@
-function ProductCard(){
+import './ProductCard.styles.scss';
+import image from '../../assets/scarecrow.svg';
+
+function ProductCard() {
   return (
-    <div>
-      <h1>Tarjeta</h1>
+    <div className="productcard">
+      <div className="productcard__content">
+        <div className="productcard__header">
+          <p className="productcard__title">Título</p>
+        </div>
+        <div className="productcard__figure">
+          <img className="productcard__image" src={image} alt="producto" />
+        </div>
+        <div className="productcard__body">
+          <span>Contador</span>
+          <button type="button">Detalles</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import Menu from '../../components/Menu/Menu';
+import ProductCard from '../../components/ProductCard/ProductCard';
 import Footer from '../../components/Footer/Footer';
+import './Home.styles.scss';
 
 function Home() {
   return (
     <>
       <Menu />
+      <div className="home__list">
+        <ProductCard />
+      </div>
       <Footer />
     </>
   );
